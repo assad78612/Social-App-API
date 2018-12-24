7de6dc21dc6f3a8e032a8c641d220ce04047d017
Assad Farid <assadfarid8@gmail.com>
2018-12-24 20:28:31 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,134 @@
+
+var express = require('express') //this is the equivalent of importng a package
+var mysql = require('mysql') //this is the equivalent of importng a package
+var bodyParser = require('body-parser');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'London12@',
+    database: 'socialapp'
+});
+
+//Difference betwen Java and JS Functions
+// [ JAVASCRIPT ]
+// function myFunction(){
+//     return "hi"
+// }
+
+// [ JAVA ]
+// private String myFunction(){
+//     return "hi"
+// }
+
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+    } else {
+        console.log('connected as id ' + connection.threadId);
+    }
+});
+
+var app = express()
+//app.use allows us to add extensions to our server
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    connection.query('SELECT * FROM `Users`', function (error, results, fields) {
+        // error will be an Error if one occurred during the query
+        // results will contain the results of the query
+        // fields will contain information about the returned results fields (if any)
+        console.log(results);
+        res.send(results);
+    });
+
+})
+
+app.post('/register', function (request, response) {
+    var username = request.body.username
+    var firstName = request.body.firstName
+    var lastName = request.body.lastName
+    var pwd = request.body.pwd
+
+
+    var userPostingData = request.body;
+
+    connection.query('SELECT * FROM Users WHERE username = "' + username + '"', function (error, results) {
+
+        if (results.length == 1) {
+            response.send("User already exists")
+        } else {
+            connection.query('INSERT INTO `Users` SET ?', userPostingData, function (error, results, fields) {
+                response.send('User inserted successfully')
+            });
+
+        }
+    });
+
+})
+
+app.get('/posts', function (req, res) {
+
+    connection.query('SELECT * FROM Post', function (error, results, fields) {
+        // console.log.apply(results);
+        res.send(results);
+    })
+})
+
+app.get('/search', function (req, res) {
+    var incomingSearch = req.query.title;
+
+    // console.log('SELECT * FROM Post WHERE PostTitle = "' + incomingSearch + '"');
+
+    connection.query('SELECT * FROM Post WHERE PostTitle = "' + incomingSearch + '"', function (error, results, fields) {
+        // console.log.apply(results);
+        res.send(results);
+    })
+})
+
+app.delete('/delete', function (req, res) {
+
+    connection.query('DELETE FROM Post WHERE PostTitle = ""', function (error, results) {
+        //console.log(results);
+
+        res.send('item deleted');
+    })
+})
+
+app.post('/login', function (req, res) {
+
+    // SELECT * FROM socialapp.Users
+    // WHERE username = "af410"
+    // AND pwd = "London23"
+
+    var username = req.body.username
+    var pwd = req.body.pwd
+
+    connection.query('SELECT * FROM Users WHERE username = "' + username + '"' + ' AND pwd = ' + '"' + pwd + '"', function (error, results) {
+
+        if (results.length == 1) {
+            res.send("Authentication successful")
+        } else {
+            res.send("Authentication unsuccessful")
+        }
+    });
+
+
+})
+
+app.get('/movies', function (req, response) {
+    connection.query("SELECT * FROM Movies", function (err, result, fields) {
+
+
+
+        response.send(result);
+
+
+
+    })
+})
+app.listen(3000)
+
