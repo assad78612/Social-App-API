df1220fb8b99033dba7b3325888f72465b3ccdf7
Assad Farid <assadfarid8@gmail.com>
2020-04-27 20:17:55 +0100

    Username shows
    Backend finished to fetch username

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json())
 app.get('/users', function (req, res) {
     var filteredUsers = [];
 
-    connection.query('SELECT * FROM `Users`', function (error, results, fields) {
+    connection.query(`SELECT * FROM Users WHERE username='${req.query.username}'`, function (error, results, fields) {
         /* error will be an Error if one occurred during the query
         results will contain the results of the query
         fields will contain information about the returned results fields (if any) */
